19a2b7723af7b030fc4b8562b539d60fdc075cf4
Dorothy Fu <dfu@GLX00159A.local>
2019-01-17 11:40:37 -0500

    Updates the flight query to be the right format when more than 1 flight is searched

--- PassengerKit/PassengerKit/AuthPath.swift
+++ PassengerKit/PassengerKit/AuthPath.swift
@@ -32,9 +32,12 @@ enum AuthPath {
             urlComponents.path = "/catalog"
 
             if let flights = query.flights, flights.count > 0 {
-                urlComponents.queryItems = [
-                    URLQueryItem(name: "flight-ids", value: flights.map({$0.id}).joined(separator: ","))
-                ]
+                var URLQueryItemArray: [URLQueryItem] = []
+                for i in flights {
+                    URLQueryItemArray += [URLQueryItem(name:"flight-ids", value: i.id)]
+                }
+                
+                urlComponents.queryItems = URLQueryItemArray
             }
         case .catalogItem(let item):
             urlComponents.path = "/product/\(item.id)"
