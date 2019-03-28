18cb19f831a814c84f25fcfa271f5899d2d3973b
Dorothy Fu <dhkfu@edu.uwaterloo.ca>
2019-03-28 15:32:06 -0400

    Moves settings elements to storyboard

--- Traveler/ViewControllers/SettingsViewController.swift
+++ Traveler/ViewControllers/SettingsViewController.swift
@@ -11,68 +11,27 @@ import TravelerKit
 import TravelerKitUI
 
 class SettingsViewController: UITableViewController {
+    @IBOutlet weak var appVersionLabel: UILabel!
+    @IBOutlet weak var deleteProfileButton: UIButton!
+    @IBOutlet weak var signOutButton: UIButton!
+
     let appVersion: String = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as? String ?? ""
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
-    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch section {
-        case 0:
-            return 2
-        case 1:
-            return 2
-        case 2:
-            return 1
-        default:
-            return 0
-        }
-    }
-
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 44
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        switch (indexPath.section, indexPath.row) {
-        case (0, 0):
-            let cell = tableView.dequeueReusableCell(withIdentifier: "infoButtonCellIdentifier", for: indexPath) as! InfoButtonCell
-            cell.label.text = "Legal Information"
-            return cell
-        case (0, 1):
-            let cell = tableView.dequeueReusableCell(withIdentifier: "infoButtonCellIdentifier", for: indexPath) as! InfoButtonCell
-            cell.label.text = "Privacy Policy"
-            return cell
-        case (1, 0):
-            let cell = tableView.dequeueReusableCell(withIdentifier: "buttonCellIdentifier", for: indexPath) as! ButtonCell
-            cell.button.setTitle("Delete Profile", for: .normal)
-            cell.button.tag = 0
-            return cell
-        case (1, 1):
-            let cell = tableView.dequeueReusableCell(withIdentifier: "buttonCellIdentifier", for: indexPath) as! ButtonCell
-            cell.button.setTitle("Sign Out", for: .normal)
-            cell.button.tag = 1
-            return cell
-        case (2, 0):
-            let cell = tableView.dequeueReusableCell(withIdentifier: "staticInfoCellIdentifier", for: indexPath) as! StaticInfoCell
-            cell.label.text = "App Version: \(appVersion)"
-            return cell
-        default:
-            fatalError("Invalid indexPath")
-        }
+        appVersionLabel.text = "App Version: \(appVersion)"
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
-
     @IBAction func onClickAlert(_ sender: UIButton) {
-        switch sender.tag {
-        case 0:
+        switch sender {
+        case deleteProfileButton:
             showAlert(withTitle:"Are you sure you want to delete your profile?", withMessage: "This will remove features & order history linked to this profile.")
-        case 1:
+        case signOutButton:
             showAlert(withTitle:"Are you sure you want to sign out of your profile?", withMessage: "Your order history will only be visible once you sign in.")
         default:
             print("Unknown Button")
