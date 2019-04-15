b776ead3bc23ff5bf7b03072e7490c2643a25c89
Dorothy Fu <dhkfu@edu.uwaterloo.ca>
2019-04-15 11:18:16 -0400

    Removes confirmation number

--- traveler-ios-ui/TravelerKitUI/ViewControllers/ReceiptViewController.swift
+++ traveler-ios-ui/TravelerKitUI/ViewControllers/ReceiptViewController.swift
@@ -21,10 +21,11 @@ class ReceiptViewController: UIViewController {
         super.viewDidLoad()
 
         // TODO: This VC should account for multiple products
+        // TODO: Add back confirmation number label after backend figures out how to send us an ORDER confirmation number
 
         titleLabel.text = receipt?.order.products.first?.title
         dateLabel.text = receipt.flatMap { DateFormatter.longFormatter.string(from: $0.order.createdDate) }
-        confirmationLabel.text = receipt?.order.referenceNumber
+        //confirmationLabel.text = receipt?.order.referenceNumber
         emailLabel.text = receipt?.order.email.email
     }
 }
