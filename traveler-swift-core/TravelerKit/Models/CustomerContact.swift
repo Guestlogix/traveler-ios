137c584aa662ad2d41fdd0bb09affda3b4fe3149
Dorothy Fu <dhkfu@edu.uwaterloo.ca>
2019-04-17 14:44:18 -0400

    Update CustomerContact model to match Android

--- traveler-swift-core/TravelerKit/Models/CustomerContact.swift
+++ traveler-swift-core/TravelerKit/Models/CustomerContact.swift
@@ -13,11 +13,11 @@ public struct CustomerContact: Decodable {
     /// Their title. e.g. "Mr" or "Mrs"
     public let title: String?
     /// Their first name
-    public let firstName: String
+    public let firstName: String?
     /// Their last name
-    public let lastName: String
+    public let lastName: String?
     /// Their email address
-    public let email: String?
+    public let email: String
     /// Their phone number
     public let phone: String?
 }
