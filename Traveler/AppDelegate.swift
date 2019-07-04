7ca9b7cdf541950c427335a949d852ecbc6b2cd9
Ata N <anamvari@guestlogix.net>
2019-07-04 10:35:48 -0400

    Updates GoogleSignIn server key

--- Traveler/AppDelegate.swift
+++ Traveler/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // GoogleSignInSDK
 
         GIDSignIn.sharedInstance()?.clientID = "722316764255-7n0gp3v0061nh12t7ghckg969orp6rc3.apps.googleusercontent.com"
-        GIDSignIn.sharedInstance()?.serverClientID = "722316764255-vvftq8tt9k45qg320gtbmgdu0jcao1th.apps.googleusercontent.com"
+        GIDSignIn.sharedInstance()?.serverClientID = "722316764255-b5b818k7flnugh4brq17r6jmqt71cddr.apps.googleusercontent.com"
 
         // TravelerSDK
 
