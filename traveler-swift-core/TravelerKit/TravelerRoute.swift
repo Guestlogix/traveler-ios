fd8d32221611824e3b415dccd2544a57ef398d53
Omar Padierna <opadierna@guestlogix.com>
2019-07-05 16:05:19 -0400

    Adds app logo + Changes base url to release

--- traveler-swift-core/TravelerKit/TravelerRoute.swift
+++ traveler-swift-core/TravelerKit/TravelerRoute.swift
@@ -15,7 +15,7 @@ enum PassengerRoute {
 
 extension PassengerRoute: Route {
     var baseURL: URL {
-        return URL(string: "https://traveler.dev.guestlogix.io")!
+        return URL(string: "https://traveler.rc.guestlogix.io/")!
     }
 
     var urlRequest: URLRequest {
