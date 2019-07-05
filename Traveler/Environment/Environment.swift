946a08da42758fa812c839fbe26b138eea5963fc
Omar Padierna <opadierna@guestlogix.com>
2019-07-05 12:24:14 -0400

    Renames folder and struct to environment

---
+++ Traveler/Environment/Environment.swift
@@ -0,0 +1,15 @@
+//
+//  Environment.swift
+//  Traveler
+//
+//  Created by Omar Padierna on 2019-07-05.
+//  Copyright © 2019 GuestLogix. All rights reserved.
+//
+
+import Foundation
+
+struct Environment {
+    static var gIDClientId: String = "$(GID_CLIENT_ID)"
+    static var gIDServerClientID: String = "$(GID_SERVER_CLIENT_ID)"
+    static var travelerKitKey: String = "$(TRAVELER_KIT_API_KEY)"
+}
