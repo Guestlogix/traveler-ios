09b50ee08ff812ac21d46e10f788dde274552d3b
Omar Padierna <opadierna@guestlogix.com>
2019-07-17 18:07:16 -0400

    Adds Supplier to CatalogItemDetails + Adds supplier model + Adds Trademark model

---
+++ traveler-swift-core/TravelerKit/Models/Supplier.swift
@@ -0,0 +1,25 @@
+//
+//  Supplier.swift
+//  TravelerKit
+//
+//  Created by Omar Padierna on 2019-07-17.
+//  Copyright © 2019 Ata Namvari. All rights reserved.
+//
+
+import Foundation
+
+/// Holds information about a supplier
+public struct Supplier: Decodable {
+    /// The identification `String`
+    public let id: String
+    /// The `Supplier`s name
+    public let name: String
+    /// The `Supplier`s trademark
+    public let trademark: Trademark?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case trademark
+    }
+}
