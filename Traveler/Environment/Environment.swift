a0a9f5e3ebf7ea405512964135c3ac19cf2d247f
Omar Padierna <opadierna@guestlogix.com>
2019-07-30 11:54:54 -0400

    Modifies Environment file to comply with local shell script injection + Modifies stripe payment provider logic to make init signature less confusing +  Adds conditional compiling to App delegate

--- Traveler/Environment/Environment.swift
+++ Traveler/Environment/Environment.swift
@@ -9,7 +9,15 @@
 import Foundation
 
 struct Environment {
-    static var gIDClientId: String = "$(GID_CLIENT_ID)"
-    static var gIDServerClientID: String = "$(GID_SERVER_CLIENT_ID)"
-    static var travelerKitKey: String = "$(TRAVELER_KIT_API_KEY)"
+
+    static var gIDClientId: String = "$(GID_CLIENT_ID)"//$(GID_CLIENT_ID)
+    static var gIDServerClientID: String = "$(GID_SERVER_CLIENT_ID)"//$(GID_SERVER_CLIENT_ID)
+
+    #if GCREW
+    static var travelerKitKey: String = "$(TRAVELER_KIT_API_KEY)"//$(TRAVELER_KIT_API_KEY)
+    #endif
+
+    #if TRAVELER
+    static var travelerKitKey: String = "$(TRAVELER_KIT_API_TEST_KEY)"//$(TRAVELER_KIT_API_TEST_KEY)
+    #endif
 }
