6b29989dd76529dc3f276054d9b068c1b8bfe1b7
Ben Ruan <m4ruanuwaterloo@gmail.com>
2019-08-29 15:51:10 -0400

    Changes back environment variables values.

--- Traveler/Environment/Environment.swift
+++ Traveler/Environment/Environment.swift
@@ -10,14 +10,14 @@ import Foundation
 
 struct Environment {
 
-    static var gIDClientId: String = "722316764255-7n0gp3v0061nh12t7ghckg969orp6rc3.apps.googleusercontent.com"//$(GID_CLIENT_ID)
-    static var gIDServerClientID: String = "722316764255-b5b818k7flnugh4brq17r6jmqt71cddr.apps.googleusercontent.com"//$(GID_SERVER_CLIENT_ID)
+    static var gIDClientId: String = "$(GID_CLIENT_ID)"//$(GID_CLIENT_ID)
+    static var gIDServerClientID: String = "$(GID_SERVER_CLIENT_ID)"//$(GID_SERVER_CLIENT_ID)
 
     #if GCREW
-    static var travelerKitKey: String = "pub_nGuuZFGuVuk1MVMKhFV1Gn1V1vGKuvFhhhk1_"//$(TRAVELER_KIT_API_KEY)
+    static var travelerKitKey: String = "$(TRAVELER_KIT_API_KEY)"//$(TRAVELER_KIT_API_KEY)
     #endif
 
     #if TRAVELER
-    static var travelerKitKey: String = "testtesttesttesttest"//$(TRAVELER_KIT_API_TEST_KEY)
+    static var travelerKitKey: String = "$(TRAVELER_KIT_API_TEST_KEY)"//$(TRAVELER_KIT_API_TEST_KEY)
     #endif
 }
