c2b26527854e2c523312fcff8aab345632652e75
Rakin Hoque <rhoque@guestlogix.com>
2019-11-07 12:38:39 -0500

    Adds dark mode support to TravelerKitUI and sample application

--- traveler-ios-ui/TravelerKitUI/ModelExtensions/CatalogItemDetails+TravelerUI.swift
+++ traveler-ios-ui/TravelerKitUI/ModelExtensions/CatalogItemDetails+TravelerUI.swift
@@ -18,6 +18,11 @@ extension CatalogItemDetails {
         let attr = try? NSMutableAttributedString(data: description.data(using: .utf8)!,
                                                   options: [.documentType : NSAttributedString.DocumentType.html],
                                                   documentAttributes: nil)
+        
+        if #available(iOS 13.0, *) {
+            // This is to maintain the label color when using attributed strings in dark mode
+            attr?.addAttribute(.foregroundColor, value: UIColor.label, range: NSRange(location: 0, length: attr!.string.count))
+        }
 
         return attr
     }
@@ -28,6 +33,11 @@ extension CatalogItemDetails {
         }
 
         let attr = try? NSMutableAttributedString(data: termsAndConditions.data(using: .utf8)!, options: [.documentType: NSMutableAttributedString.DocumentType.html], documentAttributes: nil)
+        
+        if #available(iOS 13.0, *) {
+            // This is to maintain the label color when using attributed strings in dark mode
+            attr?.addAttribute(.foregroundColor, value: UIColor.label, range: NSRange(location: 0, length: attr!.string.count))
+        }
 
         return attr
     }
