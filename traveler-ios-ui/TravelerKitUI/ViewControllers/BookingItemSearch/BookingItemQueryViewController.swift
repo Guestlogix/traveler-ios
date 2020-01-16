5bb8fee7eb6c781b52dc08d21510762541f895b9
Omar Padierna <opadierna@guestlogix.com>
2020-01-15 21:40:15 -0500

    Adds logic to BookingQueryViewController to handle textfields blocked by keyboard

--- traveler-ios-ui/TravelerKitUI/ViewControllers/BookingItemSearch/BookingItemQueryViewController.swift
+++ traveler-ios-ui/TravelerKitUI/ViewControllers/BookingItemSearch/BookingItemQueryViewController.swift
@@ -10,15 +10,79 @@ import UIKit
 import TravelerKit
 
 open class BookingItemQueryViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var cityField: UITextField!
     @IBOutlet weak var keywordsField: UITextField!
-    
+    @IBOutlet weak var searchButtonTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var searchButtonBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var searchButton: UIButton!
+
     public var query: BookingItemQuery?
     public var categories = BookingItemCategory.allCases
     
     public override func viewDidLoad() {
         cityField.text = query?.city
         keywordsField.text = query?.text
+
+        scrollView.keyboardDismissMode = .onDrag
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChangeFrame(_:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow(_:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide(_:)), name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+
+    @objc
+    func keyboardDidShow(_ note: Notification) {
+        guard let keyboardFrame = note.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
+            return
+        }
+
+        let activeField: UITextField? = [cityField, keywordsField].first { $0.isFirstResponder
+        }
+
+        let currentViewFrame = self.view.frame
+
+        if let activeField = activeField {
+            var offset = activeField.frame.origin.y - keyboardFrame.size.height
+
+            if currentViewFrame.contains(activeField.frame.origin) && offset > 0 {
+
+                //Check if active text field is the last one and if so, include button in offset.
+                if searchButtonTopConstraint.secondItem as? UITextField == activeField {
+                    offset = offset + searchButtonBottomConstraint.constant + searchButton.frame.size.height
+                }
+
+                let scrollPosition = CGPoint(x: 0, y: offset)
+                scrollView.setContentOffset(scrollPosition, animated: true)
+            }
+        }
+    }
+
+    @objc
+    func keyboardDidHide(_ note: Notification) {
+        scrollView.contentInset  = UIEdgeInsets.zero
+        scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+    }
+
+    @objc
+    func keyboardWillChangeFrame(_ note: Notification) {
+        guard let keyboardFrame = note.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
+            return
+        }
+
+        let viewControllerInView = view.convert(self.view.frame, from: nil)
+        let keyboardFrameInView = view.convert(keyboardFrame, from: nil)
+        let bottomInset = viewControllerInView.intersection(keyboardFrameInView).height + 30
+
+        self.additionalSafeAreaInsets = UIEdgeInsets(top: 0, left: 0, bottom: bottomInset, right: 0)
+
+        self.view.layoutIfNeeded()
     }
     
     public override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
