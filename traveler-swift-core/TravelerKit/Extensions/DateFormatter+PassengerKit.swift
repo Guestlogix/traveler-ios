754e0fac7c3353c2acae24614cece5dc18dd2c0b
Rakin Hoque <rhoque@guestlogix.com>
2020-02-25 12:07:02 -0500

    Always use iso8601 calendar in core SDK

--- traveler-swift-core/TravelerKit/Extensions/DateFormatter+PassengerKit.swift
+++ traveler-swift-core/TravelerKit/Extensions/DateFormatter+PassengerKit.swift
@@ -12,24 +12,28 @@ extension DateFormatter {
 
     public static var withFullDate: DateFormatter = {
         let dateFormatter = DateFormatter()
+        dateFormatter.calendar = Calendar(identifier: .iso8601)
         dateFormatter.dateFormat = "yyyymmdd"
         return dateFormatter
     }()
 
     public static var yearMonthDay: DateFormatter = {
         let dateFormatter = DateFormatter()
+        dateFormatter.calendar = Calendar(identifier: .iso8601)
         dateFormatter.dateFormat = "yyyy/MM/dd"
         return dateFormatter
     }()
 
     public static var withoutTimezone: DateFormatter = {
         let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .iso8601)
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
         return formatter
     }()
 
     public static var timeFormatter: DateFormatter = {
         let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .iso8601)
         formatter.locale = Locale.current
         formatter.dateStyle = .none
         formatter.timeStyle = .short
@@ -38,24 +42,28 @@ extension DateFormatter {
 
     public static var longFormatter: DateFormatter = {
         let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .iso8601)
         formatter.dateFormat = "EEEE, MMMM d, yyyy"
         return formatter
     }()
 
     public static var monthAsTextFormatter: DateFormatter = {
         let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .iso8601)
         formatter.dateFormat = "MMMM d, yyyy"
         return formatter
     }()
 
     public static var dateOnlyFormatter: DateFormatter = {
         let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .iso8601)
         formatter.dateFormat = "yyyy-MM-dd"
         return formatter
     }()
 
     public static var shortDisplayFormatter: DateFormatter = {
         let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .iso8601)
         formatter.dateStyle = .short
         formatter.timeStyle = .short
         return formatter
@@ -63,6 +71,7 @@ extension DateFormatter {
 
     public func fullFormatter(with timeZone: TimeZone) -> DateFormatter {
         let dateFormatter = self.copy() as! DateFormatter
+        dateFormatter.calendar = Calendar(identifier: .iso8601)
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
         dateFormatter.timeZone = timeZone
         return dateFormatter
